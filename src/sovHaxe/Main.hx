a0d21cced136b852b9a3c12fbfd9145e1d7ca250
Zach Higgs <compgeek223@gmail.com>
2021-10-02 16:49:58 -0300

    Move over to Heaps for the map renderer as It allows the Map itself to be an independant app while also still allowing me to modify the site itself.

--- src/sovHaxe/Main.hx
+++ src/sovHaxe/Main.hx
@@ -1,7 +1,12 @@
 package sovHaxe;
 
-class Main {
+class Main extends hxd.App {
+    override function init(){
+        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+        tf.text = "App Ok";
+    }
+
     public static function main() {
-        trace("ok");
+        new Main();
     }
 }
