06255d71f24ff18d369e0e3fc2a787b9a9eed49a
Zach Higgs <compgeek223@gmail.com>
2021-10-03 23:40:35 -0300

    Some Eve Swagger interface work

--- src/Backend/swagger/v1/sovreignty/Map.hx
+++ src/Backend/swagger/v1/sovreignty/Map.hx
@@ -4,6 +4,7 @@ import DateTools;
 import haxe.Http;
 import haxe.io;
 import sys.FileSystem;
+import sys.io.File;
 import sys.FileStat;
 import DateTools;
 
@@ -25,31 +26,51 @@ class Map {
 
 
     private var reqInstance:Http;
-
-    //Create a main function for if this is Used as a "Web Method" (I.E Create a Page that can be called upon RESTfully and store a Resource in the WebServer itself)
-    //Do this cleverly by essentially just creating a Haxe object of this and returning the results in the objects response
+    private var response(null, null):String;
+    /**
+    *     Create a main function for if this is Used as a "Web Method" (I.E Create a Page that can be called upon RESTfully and store a Resource in the WebServer itself)
+    *       Do this cleverly by essentially just creating a Haxe object of this and returning the results in the objects response.static
+    *
+    *       only ever really needed if compiled as a Neko Main File since you want it to act as its own "Module" due to how I designed this app
+    **/
 
     public static function main() {
-        Sys.println(new Map().response);
+        Sys.println(new Map());
     }
     /**
     * Create a new function for if this Used as a Haxe method
     **/
     public function new() {
-        filePulse();
+       return filePulse();
     }
 
     /**
     * Run the resourcce pulse to check how recent it was created and Act as the main way to create and act as a Default way to Check the pulse and handle the pulse
     *
-    * Override this method and call
+    * This function can be redefined for different pulse conditions but this isn't necssary unless you need to quickly fix for an API update. See source for how to do this easily.
+    * @return String - response content
     **/
 
-    function filePulse(){
+    dynamic function filePulse():String{
+        // If we've already been running
         if(sys.FileSystem.exists(resourcePath.toString())){
             var resourceStat:FileStat =  sys.FileSystem.stat(resourcePath.toString());
+            var resourceStaleTime = DateTools.delta(resourceStat.mtime, DateTools.seconds(remoteEndpointCacheTime * antiRateLimitingMultiplier));
 
+            if(Date.now().getTime() > resourceStaleTime){
+                //Resource is stale, refresh it
+                
+            } else {
+                // If the resource is not stale. just le
+                return this.response;
+            }
+        } else {
+            this.response = request();
+            File.saveContent(resourcePath.toString(), this.response);
+            return this.response;
         }
+
+
     }
 
     /**
@@ -57,7 +78,7 @@ class Map {
     * @return response
     **/
     function request(){
-        var response:String;
-        reqInstance = new Http(endpointURL);
+        this.reqInstance = new Http(endpointURL);
+        return response;
     }
 }
